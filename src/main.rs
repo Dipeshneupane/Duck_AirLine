9279e2bc356f7915d2fb2469decdf05d46828630
Dipesh Neupane <dipeshneupane2055@gmail.com>
2023-06-06 15:27:17 +0545

    used control flow to calculate distance between different location

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,46 @@
 fn main(){
     const EARTH_RADIUS_IN_KILOMETERS: f64 = 6371.0;
 
-    let kcle_latitude_degrees :f64 = 41.4075;
-    let kcle_longitude_degrees :f64 = -81.851111;
+    let route = [
+        ("KCLE", 41.4075, -81.851111),
+        ("LEYIR", 41.51030, -83.88080),
+        ("PIONS", 41.65390, -84.48190),
+        ("ZOSER", 41.72390, -84.78130),
+        ("MODEM", 41.72800, -84.89730),
+        ("BRYTO", 41.74170, -85.31320),
+        ("FOD", 42.61110, -94.29480),
+        ("KSLC", 40.7861, -111.9822)
+    ];
 
-    let kslc_latitude_degrees :f64 = 40.7861;
-    let kslc_longitude_degrees:f64 = -111.9822;
+    let mut total_distance = 0.0;
+    let mut previous_waypoint : Option<(&str, f64, f64)> = None;
 
-    let kcle_latitude_radians :f64 = kcle_latitude_degrees.to_radians();
-    let kslc_latitude_radians :f64 = kslc_latitude_degrees.to_radians();
+    for waypoint in route.iter(){
+        match previous_waypoint {
+            None => {
+                previous_waypoint = Option::from(waypoint.clone());
+                continue;
+            }
+            Some(previous_waypoint_value) => {
+                let previous_waypoint_radian = previous_waypoint_value.1.to_radians();
+                let waypoint_radians = waypoint.1.to_radians();
 
-    let delta_latitude = (kcle_latitude_degrees - kslc_latitude_degrees).to_radians();
-    let delta_longitude = (kcle_longitude_degrees - kslc_longitude_degrees).to_radians();
+                let delta_latidute = (previous_waypoint_value.1 - waypoint.1).to_radians();
+                let delta_longitude = (previous_waypoint_value.2 - waypoint.2).to_radians();
 
-    let inner_central_angle = f64::powi((delta_latitude / 2.0).sin(),2)
-        + kcle_latitude_radians.cos()
-        * kslc_latitude_radians.cos()
-        * f64::powi((delta_longitude / 2.0).sin(),2);
+                let inner_central_angle = f64::powi((delta_latidute/2.0).sin(),2)
+                    + previous_waypoint_radian.cos() * waypoint_radians.cos()
+                    * f64::powi((delta_longitude/2.0).sin(),2);
 
-    let central_angle:f64 = 2.0 * inner_central_angle.sqrt().asin();
+                let central_angle = 2.0 * inner_central_angle.sqrt().asin();
+                let distance = EARTH_RADIUS_IN_KILOMETERS * central_angle;
 
-    let distance:f64 = EARTH_RADIUS_IN_KILOMETERS * central_angle;
+                total_distance += distance;
+                previous_waypoint = Option::from(waypoint.clone());
 
-    println!("The distance between the two points is {:.2} kilometers", distance);
+                println!("The distance between {} and {} is {:.1} kilometers", previous_waypoint_value.0, waypoint.0, distance);
 
+            }
+        }
+    }
 }
